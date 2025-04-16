91bc172008e91f06898f347105282be338def191
Baldi <d.baldischwieler@t-online.de>
2025-04-16 18:38:41 +0200

    fix: Remove unused LogChannel from Config.cs

--- DiscordBot/Config.cs
+++ DiscordBot/Config.cs
@@ -17,11 +17,6 @@ namespace DiscordBot
         public static string Token =>
             Settings["Token"] ?? throw new MissingFieldException("Token is needed to start the bot");
 
-        // ATM I just use a set log channel from my testing guild. But the log channel should be set on a per-guild basis
-        public static ulong LogChannelId => ulong.Parse(Settings["LogChannelId"] ??
-                                                        throw new MissingFieldException(
-                                                            "Log channel id is needed (atm) to start the bot"));
-
         public static ulong DebugGuildId => ulong.Parse(Settings["DebugGuildId"] ??
                                                         throw new MissingFieldException(
                                                             "Debug guild id is needed (atm) to start the bot"));
